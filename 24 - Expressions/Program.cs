77513c9f5673155d67317f051cec422e3ba74148
michaelnielsendk <michael@mcrillo.dk>
2020-02-08 22:46:15 +0100

    Add project files.

---
+++ 24 - Expressions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wincubate.Module12.Slide24
+{
+
+
+
+
+    public class Person
+    {
+        public string FirstName { get; set; } = "Jens";
+        public string LastName { get; set; } = "Jensen";
+        public int Age { get; set; } = 0;
+        public string FullName => $"{FirstName} {LastName}";
+        public override string ToString() =>
+        $"{FullName} is {Age} year{(Age == 1 ? "" : "s")} old";
+    }
+
+
+    class Program
+    {
+      
+
+        static void Main( string[] args )
+        {
+            Person mig = new Person();
+            mig.FirstName = "Michael";
+      
+
+            Console.WriteLine(mig.ToString());
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
